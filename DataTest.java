aafb8a14cba1d212903b44c7bcc135eab5d24d37
bobobo00 <48381156+bobobo00@users.noreply.github.com>
2019-11-01 23:06:54 +0800

    数据流操作

--- DataTest.java
+++ DataTest.java
@@ -9,7 +9,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
- * 数据流
+ * 数据流    操作对象-》基本数据类型和String
  * 1，写出后读取
  * 2，读取的顺序与写入的顺序一致。
  * @author dell
@@ -19,15 +19,17 @@ public class DataTest {
 	public static void main(String[] args) throws IOException {
 		//写出
 		ByteArrayOutputStream baos=new ByteArrayOutputStream();
-		DataOutputStream dos=new DataOutputStream(baos);
-		
+		DataOutputStream dos=new DataOutputStream(new BufferedOutputStream(baos));
+		//操作数据类型+数据
 		dos.writeUTF("尚学堂");
 		dos.writeInt(18);
 		dos.writeChar('a');
 		dos.writeFloat(1.8F);
+		dos.flush();
 		byte[] datas=baos.toByteArray();
+		System.out.println(datas.length);
 		//读取
-		DataInputStream bis=new DataInputStream(new ByteArrayInputStream(datas));
+		DataInputStream bis=new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(datas)));
 		//顺序与写出一致
 		System.out.println(bis.readUTF()+" "+bis.readInt()+" "+bis.readChar()+" "+bis.readFloat());
 	}
